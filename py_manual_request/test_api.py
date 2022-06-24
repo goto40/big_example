94adbaa4feaee5e8fe384c5660fe3fdce8b590d0
goto40 <pierre.bayerl@googlemail.com>
2022-06-24 10:34:39 +0000

    added python test (no code gen)

--- py_manual_request/test_api.py
+++ py_manual_request/test_api.py
@@ -1,2 +1,23 @@
+import requests
+import json
+
 def test_api():
-    assert 1==1
+    put_data={'message':'Max'}
+    headers = {"Content-Type": "application/json"}
+    response = requests.put('http://localhost:8000/data', data=json.dumps(put_data), headers=headers)
+    assert response.status_code == 200
+
+    response = requests.get('http://localhost:8000/data')
+    assert response.status_code == 200
+    data = json.loads(response.content)
+    assert data["message"] == "Max"
+
+    put_data={'message':'Moriz'}
+    headers = {"Content-Type": "application/json"}
+    response = requests.put('http://localhost:8000/data', data=json.dumps(put_data), headers=headers)
+    assert response.status_code == 200
+
+    response = requests.get('http://localhost:8000/data')
+    assert response.status_code == 200
+    data = json.loads(response.content)
+    assert data["message"] == "Moriz"
