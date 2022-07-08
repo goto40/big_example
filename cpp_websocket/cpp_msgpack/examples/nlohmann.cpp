3319ee6047292f9d6f0b7e766f8e5e11631bde39
goto40 <pierre.bayerl@googlemail.com>
2022-07-08 14:25:18 +0000

    next steps: added nlohmann,  msgpack

---
+++ cpp_websocket/cpp_msgpack/examples/nlohmann.cpp
@@ -0,0 +1,22 @@
+#include <nlohmann/json.hpp>
+#include <iostream>
+
+int main() {
+  nlohmann::json j2 = {
+        {"pi", 3.141},
+        {"happy", true},
+        {"name", "Niels"},
+        {"nothing", nullptr},
+        {"answer", {
+            {"everything", 42}
+        }},
+        {"list", {1, 0, 2}},
+        {"object", {
+            {"currency", "USD"},
+            {"value", 42.99}
+        }}
+    };
+    
+    std::cout << j2;
+
+}
---
+++ cpp_websocket/cpp_msgpack/src/websockets_with_msgpack/version.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+namespace big_example {
+
+extern const std::string VERSION;
+extern const std::string VERSION_SHORT;
+
+}
