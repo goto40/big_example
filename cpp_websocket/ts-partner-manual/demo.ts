0a739e710746c91c6fb28d73934eadd257480170
goto40 <pierre.bayerl@googlemail.com>
2022-07-08 16:24:09 +0000

    playing

--- cpp_websocket/ts-partner-manual/demo.ts
+++ cpp_websocket/ts-partner-manual/demo.ts
@@ -1,33 +1,23 @@
-import {w3cwebsocket} from 'websocket'
+import {WebSocket} from 'isomorphic-ws'
 
 // npx tsx demo.ts 
 console.log("Websocket demo")
 
-const client = new w3cwebsocket('ws://127.0.0.1:9090/','web');
+const ws = new WebSocket('ws://127.0.0.1:9090/');
 
-client.onerror = function() {
-    console.log('Connection Error');
+ws.onopen = function open() {
+  console.log('connected');
+  ws.send(Date.now());
 };
 
-client.onopen = function() {
-    console.log('WebSocket Client Connected');
-
-    function sendNumber() {
-        if (client.readyState === client.OPEN) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            client.send(number.toString());
-            setTimeout(sendNumber, 1000);
-        }
-    }
-    sendNumber();
+ws.onclose = function close() {
+  console.log('disconnected');
 };
 
-client.onclose = function() {
-    console.log('echo-protocol Client Closed');
-};
+ws.onmessage = function incoming(data) {
+  console.log(`Roundtrip time: ${Date.now() - data.data} ms`);
 
-client.onmessage = function(e) {
-    if (typeof e.data === 'string') {
-        console.log("Received: '" + e.data + "'");
-    }
+  setTimeout(function timeout() {
+    ws.send(Date.now());
+  }, 500);
 };
