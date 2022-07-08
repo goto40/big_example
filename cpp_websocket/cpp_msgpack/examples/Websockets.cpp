e0a4a81d98cd78b28ddba395bbadc743a38124f8
goto40 <pierre.bayerl@googlemail.com>
2022-07-08 16:22:54 +0000

    ws playground

--- cpp_websocket/cpp_msgpack/examples/Websockets.cpp
+++ cpp_websocket/cpp_msgpack/examples/Websockets.cpp
@@ -1,4 +1,33 @@
-#include <iostream>
+#include <seasocks/Server.h>
+#include <seasocks/WebSocket.h>
+#include <seasocks/PrintfLogger.h>
+#include <memory>
+
+
+class MyHandler : public seasocks::WebSocket::Handler {
+public:
+    void onConnect(seasocks::WebSocket* /*connection*/) override {
+        std::cout << "connect\n";
+    }
+
+    void onData(seasocks::WebSocket* connection, const uint8_t* data, size_t length) override {
+        connection->send(data, length); // binary
+    }
+
+    void onData(seasocks::WebSocket* connection, const char* data) override {
+        connection->send(data); // text
+    }
+
+    void onDisconnect(seasocks::WebSocket* /*connection*/) override {
+    }
+};
+
 int main() {
-    std::cout << "main\n";
+    auto logger = std::make_shared<seasocks::PrintfLogger>();
+    seasocks::Server server(logger);
+
+    auto handler = std::make_shared<MyHandler>();
+    server.addWebSocketHandler("/", handler);
+    
+    server.serve("web", 9090);
 }
---
+++ cpp_websocket/ts-partner-manual/demo.ts
@@ -0,0 +1,33 @@
+import {w3cwebsocket} from 'websocket'
+
+// npx tsx demo.ts 
+console.log("Websocket demo")
+
+const client = new w3cwebsocket('ws://127.0.0.1:9090/','web');
+
+client.onerror = function() {
+    console.log('Connection Error');
+};
+
+client.onopen = function() {
+    console.log('WebSocket Client Connected');
+
+    function sendNumber() {
+        if (client.readyState === client.OPEN) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            client.send(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+};
+
+client.onclose = function() {
+    console.log('echo-protocol Client Closed');
+};
+
+client.onmessage = function(e) {
+    if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'");
+    }
+};
